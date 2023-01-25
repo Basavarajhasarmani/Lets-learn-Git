7ca8f5ee3f7fd3dfe2237c1a2804b4786575944a
Basavaraj <basavraj.hasarmani@gmail.com>
2023-01-25 16:50:31 +0530

    Some print statement is written

--- Diff.java
+++ Diff.java
@@ -1,3 +1,7 @@
 public class Diff{
-    
+    public static void main(String[] args){
+        int a= 10;
+        int b = 20;
+        System.out.println("The difference is " + ( b - a));
+    }
 }
